04134a58d4a3f4b7ae4bf025dcece6b54b17e85f
Victor Vasconcellos <victorvasconcellos42@gmail.com>
2023-02-14 09:09:59 +0000

    System - Add header libft and change minishell header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 04:14:17 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/02/14 04:15:53 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/14 09:08:29 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "libft/libft.h"
+# include "libft.h"
 
 #endif
