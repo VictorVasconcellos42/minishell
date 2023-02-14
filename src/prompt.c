f7a9fe99cc91ff6b9c82c9cb7c22c27c95c1da0b
vde-vasc <vde-vasc@student.42.rio>
2023-02-14 09:45:09 -0300

    Feature - Add history in minishell

--- src/prompt.c
+++ src/prompt.c
@@ -6,16 +6,25 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:06:42 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/02/14 04:18:23 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/14 09:19:03 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <string.h>
 
 int	main(void)
 
 {
+	char *input;
 	while (1)
-		readline("minishell >> ");
+	{
+		input = readline("minishell >> ");
+		add_history(input);
+		if (!strcmp(input, "exit"))
+			break ;
+		free(input);
+	}
+	free(input);
 	return (0);
 }
