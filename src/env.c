b9fdc804fbc14c6d24f99769afbdb1d917f18571
vde-vasc <vde-vasc@student.42.rio>
2023-02-16 11:42:26 -0300

    Feature - Create env builtin

--- src/env.c
+++ src/env.c
@@ -3,11 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 16:30:08 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/02/14 16:30:26 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/16 11:41:17 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static int	env_size(char **str)
+
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void    create_env(t_cmd *cmd, char **envp)
+
+{
+    int i;
+
+    i = 0;
+    cmd->env_size = env_size(envp);
+    cmd->env = malloc(sizeof(char *) * (cmd->env_size + 1));
+    while (i < cmd->env_size - 1)
+    {
+        cmd->env[i] = envp[i];
+        printf("%s\n", cmd->env[i++]);
+    }
+    i = 0;
+}
