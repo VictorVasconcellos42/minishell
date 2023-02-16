4956183b6890d4828a700e333cb7367d7b269e17
vde-vasc <vde-vasc@student.42.rio>
2023-02-16 11:43:07 -0300

    Refactor - add env function in main

--- src/prompt.c
+++ src/prompt.c
@@ -6,11 +6,17 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:06:42 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/02/16 02:01:12 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/16 11:39:53 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static int	is_builtin(char *input)
+
+{
+	return (!strncmp(input, "env", -1) || !strncmp(input, "echo", -1));
+}
 
 int	len(char **str)
 
@@ -31,22 +37,12 @@ int	main(int argc, char **argv, char **envp)
 	i = 0;
 	while (1)
 	{
-		cmd.input = readline("minishell >> ");
+		cmd.input = readline("$ ");
 		add_history(cmd.input);
 		if (!strcmp(cmd.input, "exit"))
 			break ;
-		if (!strcmp(cmd.input, "env"))
-		{
-			cmd.env_size = len(envp);
-			cmd.env = malloc(sizeof(char *) * cmd.env_size + 1);
-			while (i < cmd.env_size - 1)
-			{
-				cmd.env[i] = envp[i];
-				printf("%s\n", envp[i]);
-				i++;
-			}
-			i = 0;
-		}
+		if (is_builtin(cmd.input))
+			create_env(&cmd, envp);
 		free(cmd.input);
 	}
 	i = 0;
