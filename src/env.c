4e90d5ad1bc5d48454d44b70c72bcf36ba53869c
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2023-02-18 01:43:25 +0000

    Fix - now, create env only create

--- src/env.c
+++ src/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 16:30:08 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/02/16 13:01:10 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/02/18 01:32:16 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,9 @@ void	create_env(t_cmd *cmd, char **envp)
 {
 	int	i;
 
-	i = 0;
+	i = -1;
 	cmd->env_size = env_size(envp);
 	cmd->env = malloc(sizeof(char *) * (cmd->env_size + 1));
-	while (i < cmd->env_size - 1)
-	{
-		cmd->env[i] = envp[i];
-		printf("%s\n", cmd->env[i++]);
-	}
-	i = 0;
+	while (++i < cmd->env_size - 1)
+		cmd->env[i] = ft_strdup(envp[i]);
 }
