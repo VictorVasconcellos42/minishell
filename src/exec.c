88167df7090a9df6e08c7ed98b3f922cf64fd4c4
vde-vasc <vde-vasc@student.42.rio>
2023-02-28 07:24:15 -0300

    Feature - Create first execve function

---
+++ src/exec.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 02:43:16 by vde-vasc          #+#    #+#             */
+/*   Updated: 2023/02/28 03:18:02 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	execution(t_cmd *exec)
+
+{
+	int	pid;
+
+	pid = fork();
+	exec->path = ft_split(exec->input, ' ');
+	if (pid == 0)
+	{
+		execve(exec->path[0], exec->path, exec->env);
+		exit(0);
+	}
+	else
+		wait(0);
+}
