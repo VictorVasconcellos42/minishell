2f57f243ab949162155dbf71ad5d2a435ef5ce30
vde-vasc <vde-vasc@student.42.rio>
2023-03-10 07:45:15 -0300

    Fix: Ctrl D segfault finish

---
+++ src/init.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 18:47:25 by vde-vasc          #+#    #+#             */
+/*   Updated: 2023/03/09 18:48:44 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void    init_minishell(t_cmd *cmd)
+
+{
+    cmd->exec = NULL;
+    cmd->env = NULL;
+    cmd->c_pipes = 0;
+    cmd->path = NULL;
+    cmd->pipex = NULL;
+    cmd->token = NULL;
+}
