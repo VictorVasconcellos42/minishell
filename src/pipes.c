6549a10947cf7cd8c508fe68a135d2da44c1b2a6
vde-vasc <vde-vasc@student.42.rio>
2023-03-17 16:18:34 -0300

    Fix (Norm): putting in norm standard

--- src/pipes.c
+++ src/pipes.c
@@ -6,41 +6,25 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 22:26:42 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/03/11 11:27:01 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/03/17 16:05:43 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	has_pipe(char *string, t_cmd *cmd)
+int	multiple_pipes(t_cmd *cmd, int i, int *pid, int **fd)
 
 {
-	int i;
-
-    i = 0;
-	cmd->c_pipes = 0;
-	while (string[i])
+	while (i < cmd->c_pipes - 1)
 	{
-		if (ft_strchr("|", string[i++]))
-			cmd->c_pipes++;
+		pipe(fd[++i]);
+		pid[i] = fork();
+		if (pid[i] == 0)
+			middle_pipe(cmd, i, fd);
+		close(fd[i - 1][0]);
+		close(fd[i][1]);
 	}
-	if (cmd->c_pipes != 0)
-	    return (TRUE);
-	return (FALSE);
-}
-
-int    **init_fd(t_cmd *cmd)
-
-{
-	int i;
-	int **fd;
-
-	i = 0;
-	cmd->pipex = ft_split(cmd->input, '|');
-    fd = malloc(sizeof(int *) * cmd->c_pipes);
-	while (i < cmd->c_pipes)
-		fd[i++] = malloc(sizeof(int) * 2);
-	return (fd);
+	return (i);
 }
 
 void	pipes(t_cmd *cmd, int i, int j)
@@ -56,24 +40,19 @@ void	pipes(t_cmd *cmd, int i, int j)
 	if (pid[i] == 0)
 		first_pipe(cmd, fd[i]);
 	close(fd[i][WRITE_END]);
-	while (i < cmd->c_pipes - 1)
-	{
-		pipe(fd[++i]);
-		pid[i] = fork();
-		if (pid[i] == 0)
-		    middle_pipe(cmd, i, fd);
-		close(fd[i - 1][0]);
-		close(fd[i][1]);
-	}
+	i = multiple_pipes(cmd, i, pid, fd);
 	pid[i + 1] = fork();
 	if (pid[i + 1] == 0)
 		last_pipe(cmd, i + 1, fd[i]);
 	close(fd[i][READ_END]);
 	while (pid[j])
 		waitpid(pid[j++], NULL, 0);
+	free(pid);
+	free_fd(fd, cmd->c_pipes);
+	cmd->c_pipes = 0;
 }
 
-void first_pipe(t_cmd *cmd, int *fd)
+void	first_pipe(t_cmd *cmd, int *fd)
 
 {
 	close(fd[READ_END]);
@@ -81,11 +60,16 @@ void first_pipe(t_cmd *cmd, int *fd)
 	close(fd[WRITE_END]);
 	cmd->exec = ft_split(cmd->pipex[0], ' ');
 	if (check_command(cmd) == FALSE)
+	{
 		error_command(cmd->exec[0]);
-	exit(127);
+		free_matriz(cmd->exec);
+		exit(127);
+	}
+	free_matriz(cmd->exec);
+	exit(EXIT_SUCCESS);
 }
 
-void middle_pipe(t_cmd *cmd, int i, int **fd)
+void	middle_pipe(t_cmd *cmd, int i, int **fd)
 
 {
 	close(fd[i][READ_END]);
@@ -95,17 +79,27 @@ void middle_pipe(t_cmd *cmd, int i, int **fd)
 	close(fd[i][WRITE_END]);
 	cmd->exec = ft_split(cmd->pipex[i], ' ');
 	if (check_command(cmd) == FALSE)
+	{
 		error_command(cmd->exec[0]);
-	exit(127);
+		free_matriz(cmd->exec);
+		exit(127);
+	}
+	free_matriz(cmd->exec);
+	exit(EXIT_SUCCESS);
 }
 
-void last_pipe(t_cmd *cmd, int i, int *fd)
+void	last_pipe(t_cmd *cmd, int i, int *fd)
 
 {
 	dup2(fd[READ_END], STDIN_FILENO);
 	close(fd[READ_END]);
 	cmd->exec = ft_split(cmd->pipex[i], ' ');
 	if (check_command(cmd) == FALSE)
+	{
 		error_command(cmd->exec[0]);
-	exit(127);
+		free_matriz(cmd->exec);
+		exit(127);
+	}
+	free_matriz(cmd->exec);
+	exit(EXIT_SUCCESS);
 }
