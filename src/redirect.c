1ecff9e4f463fb404a621db0eb66cf55037dcb5b
vde-vasc <vde-vasc@student.42.rio>
2023-04-08 01:34:19 -0300

    Feat: create append, input and heredoc

--- src/redirect.c
+++ src/redirect.c
@@ -6,15 +6,19 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 08:48:20 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/04/07 17:18:47 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/04/08 01:27:28 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	create_file(char *name)
+int	create_file(char *name, int type)
 
 {
+	if (type == APP_INPUT)
+		return (open(name, O_CREAT | O_WRONLY | O_APPEND, 0644));
+	else if (type == HERE_DOC)
+		return (open(name, O_CREAT | O_WRONLY | O_RDONLY | O_APPEND, 0666));
 	return (open(name, O_CREAT | O_WRONLY | O_TRUNC, 0644));
 }
 
@@ -23,10 +27,65 @@ void	output_action(t_sentence *table, int pos, int i)
 {
 	int	file;
 	
-	file = create_file(table[pos].args[i + 1]);
+	file = create_file(table[pos].args[i], R_OUTPUT);
 	if (table[pos].output != STDOUT_FILENO)
 		close(table[pos].output);
 	table[pos].output = file;
+}
+
+int	append_action(t_sentence *table, int pos, int i)
+
+{
+	int file;
+
+	file = create_file(table[pos].args[i], APP_INPUT);
+	if (file == -1)
+	{
+		write(1, "bash: ", 6);
+		perror(table[pos].args[i]);
+		return (file);
+	}
+	else if (table[pos].output != STDOUT_FILENO)
+		close(table[pos].output);
+	table[pos].output = file;
+	return (TRUE);
+}
+
+int	input_action(t_sentence *table, int pos, int i)
+
+{
+	int	file;
+
+	file = open(table[pos].args[i], O_RDONLY);
+	if (file == -1)
+	{
+		write(1, "bash: ", 6);
+		perror(table[pos].args[i]);
+		return (file);
+	}
+	else if (table[pos].input != STDIN_FILENO)
+		close(table[pos].input);
+	table[pos].input = file;
+	return (TRUE);
+}
+
+void	heredoc_action(t_sentence *table, int pos, int i)
+
+{
+	char	*text;
+	int		fake_file;
+	int		len;
+
+	fake_file = create_file(table[pos].args[i], HERE_DOC);
+	text = readline("> ");
+	len = ft_strlen(table[pos].args[i]);
+	while (ft_strncmp(table[pos].args[i], text, len))
+	{
+		ft_putendl_fd(text, fake_file);
+		free(text);
+		text = readline("> ");
+	}
+	input_action(table, pos, i);
 }
 
 int	is_output(char *type)
@@ -61,7 +120,7 @@ int	is_heredoc(char *type)
 	return (FALSE);
 }
 
-void	control_redirect(t_sentence *table)
+int	control_redirect(t_sentence *table)
 
 {
 	int	command;
@@ -75,21 +134,22 @@ void	control_redirect(t_sentence *table)
 		while (table[command].args[i])
 		{
 			if (is_append(table[command].args[i]))
-				return ;
-				// REDIRECIONAR E NAO APAGAR O CONTEUDO ANTIGO, A SAIDA DO COMANDO PARA UM ARQUIVO
-			else if (is_heredoc(table[command].args[i]))
-				// ABRIR UM PROMPT COM UMA PALAVRA CORINGA PARA PARA-LO.
-				return ;
-			else if (is_input(table[command].args[i]))
-				return ;
-				//REDIRECIONAR O CONTEUDO DE UM ARQUIVO PARA A ENTRADA DE UM COMANDO
-			else if (is_output(table[command].args[i]))
-			{
-				output_action(table, command, i);
-				//REDIRECIONAR O CONTEUDO DE UM COMANDO PARA DENTRO DE UM ARQUIVO
+			{	
+				if (append_action(table, command, i + 1) == -1)
+					return (FALSE);
 			}
+			else if (is_heredoc(table[command].args[i]))
+				heredoc_action(table, command, i + 1);
+			else if (is_input(table[command].args[i]))
+			{
+				if (input_action(table, command, i + 1) == -1)
+					return (FALSE);
+			}
+			else if (is_output(table[command].args[i]))
+				output_action(table, command, i + 1);
 			i++;
 		}
 		command++;
 	}
+	return (TRUE);
 }
