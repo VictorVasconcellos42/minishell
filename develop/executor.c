d8e435bc34580ee3b42608a977a764f0dd8782d2
vde-vasc <vde-vasc@student.42.rio>
2023-04-07 23:01:50 -0300

    Feat: create executor function

--- develop/executor.c
+++ develop/executor.c
@@ -6,19 +6,40 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 23:42:25 by vde-vasc          #+#    #+#             */
-/*   Updated: 2023/04/07 18:44:04 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2023/04/07 22:24:45 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 
-
-/* char	which_redirect(char *line)
+void	the_executor(t_sentence sentence, t_cmd *cmd)
 
 {
-	if (ft_strchr("><", line[i]))
-} */
+	char	**table_path;
+	char	*full_path;
+	int		i;
+
+	table_path = get_path(cmd);
+	full_path = NULL;
+	i = -1;
+	if (sentence.args[0][0] == '/' || sentence.args[0][0] == '.')
+		full_path = ft_strdup(sentence.args[0]);
+	while (table_path[++i])
+	{
+		if (access(full_path, F_OK | X_OK) == 0)
+		{
+			free(sentence.args[0]);
+			sentence.args[0] = ft_strdup(full_path);
+			execve(sentence.args[0], sentence.args, cmd->env);
+		}
+		free(full_path);
+		full_path = ft_strjoin(table_path[i], "/");
+		full_path = ft_strjoin_gnl(full_path, sentence.args[0]);
+	}
+	printf("bash: %s: command not found\n", sentence.args[0]);
+	exit(127);
+}
 
 void	redirect(t_cmd *cmd)
 
@@ -44,7 +65,7 @@ void	print_matriz(char **input)
 		printf("ARG: [%s]", input[i++]);
 }
 
-void	remove_redirect(t_sentence sentence)
+void	remove_output(t_sentence sentence)
 
 {
 	int		i;
@@ -81,11 +102,11 @@ void	execute_sentence(t_sentence sentence, t_cmd *cmd)
 		if (sentence.input != STDIN_FILENO)
 			dup2(sentence.input, STDIN_FILENO);
 		if (sentence.output != STDOUT_FILENO)
+		{
 			dup2(sentence.output, STDOUT_FILENO);
-		remove_redirect(sentence);
-		execve(sentence.args[0], sentence.args, cmd->env);
-		perror(sentence.args[0]);
-		exit(1);
+			remove_output(sentence);
+		}
+		the_executor(sentence, cmd);
 	}
 	dup2(backup, STDOUT_FILENO);
 	waitpid(pid, NULL, 0);
